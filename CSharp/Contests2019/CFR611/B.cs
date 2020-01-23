9f217be2f510d5419491006f636b8790f8bf9417
Keiho Sakapon <git@saka-pon.net>
2020-01-23 22:17:26 +0900

    Initialize

---
+++ CSharp/Contests2019/CFR611/B.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class B
+{
+	static void Main()
+	{
+		Console.ReadLine();
+		var s = Console.ReadLine();
+		var n = int.Parse(Console.ReadLine());
+		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+
+		Console.WriteLine(string.Join(" ", h));
+	}
+}
