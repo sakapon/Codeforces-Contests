e7c75f6b7b2d9cce114ee8293f80963605a3e7d9
Keiho Sakapon <git@saka-pon.net>
2020-01-23 22:11:05 +0900

    Add templates

---
+++ CSharp/Contests2019/CFR000/D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class D
+{
+	static void Main()
+	{
+		Console.ReadLine();
+		var s = Console.ReadLine();
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var h = read();
+		var ps = new int[h[0]].Select(_ => read()).ToArray();
+
+		Console.WriteLine(string.Join(" ", a));
+	}
+}
