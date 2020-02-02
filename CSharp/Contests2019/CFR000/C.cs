f579d3a3580c45feb6acf8821c2de4043b5610fb
Keiho Sakapon <git@saka-pon.net>
2020-02-02 12:06:41 +0900

    Update template

--- CSharp/Contests2019/CFR000/C.cs
+++ CSharp/Contests2019/CFR000/C.cs
@@ -3,14 +3,17 @@ using System.Linq;
 
 class C
 {
-	static void Main()
-	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 
-		Console.WriteLine(string.Join(" ", h));
+	static long Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var h = Read();
+		//int n = h[0], m = h[1];
+		var s = Console.ReadLine();
+
+		var r = 0L;
+		return r;
 	}
 }
