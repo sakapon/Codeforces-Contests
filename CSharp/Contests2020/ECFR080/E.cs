21bf588a0b4150e7f9325cd6e98b3c2fc5b49657
Keiho Sakapon <git@saka-pon.net>
2020-02-02 12:26:43 +0900

    Initialize

---
+++ CSharp/Contests2020/ECFR080/E.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class E
+{
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
+	static void Main()
+	{
+		Console.ReadLine();
+		var s = Console.ReadLine();
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var h = Read();
+		var ps = new int[h[0]].Select(_ => Read()).ToArray();
+
+		Console.WriteLine(string.Join(" ", a));
+	}
+}
