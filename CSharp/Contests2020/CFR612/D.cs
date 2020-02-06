810fe907f047aa6225c1b9688011a9efc1c9a5cc
Keiho Sakapon <git@saka-pon.net>
2020-02-06 11:28:09 +0900

    AC

--- CSharp/Contests2020/CFR612/D.cs
+++ CSharp/Contests2020/CFR612/D.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class D
@@ -6,13 +7,45 @@ class D
 	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = Read();
-		var ps = new int[h[0]].Select(_ => Read()).ToArray();
+		pcs = new int[n].Select(_ => Read()).ToArray();
+		map = GetDirectedMap(n, pcs.Select((x, i) => new[] { x[0], i + 1 }).ToArray());
 
-		Console.WriteLine(string.Join(" ", a));
+		try
+		{
+			var l = Dfs(map[0][0], 0);
+			Console.WriteLine("YES");
+			Console.WriteLine(string.Join(" ", l.Select((p, i) => (p: p, a: i + 1)).OrderBy(_ => _.p).Select(_ => _.a)));
+		}
+		catch (Exception)
+		{
+			Console.WriteLine("NO");
+		}
+	}
+
+	static int[][] pcs;
+	static List<int>[] map;
+
+	static List<int> Dfs(int p, int p0)
+	{
+		List<int> l = null;
+		foreach (var p2 in map[p])
+		{
+			if (p2 == p0) continue;
+			if (l == null) l = Dfs(p2, p);
+			else l.AddRange(Dfs(p2, p));
+		}
+
+		if (l == null) l = new List<int>();
+		l.Insert(pcs[p - 1][1], p);
+		return l;
+	}
+
+	static List<int>[] GetDirectedMap(int n, int[][] rs)
+	{
+		var map = Array.ConvertAll(new int[n + 1], _ => new List<int>());
+		foreach (var r in rs)
+			map[r[0]].Add(r[1]);
+		return map;
 	}
 }
