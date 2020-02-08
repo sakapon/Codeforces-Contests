1d8b12496dd1c9fc0fafa7708b493016cb23ba2c
Keiho Sakapon <git@saka-pon.net>
2020-02-08 23:20:22 +0900

    AC

--- CSharp/Contests2020/CFR616/A.cs
+++ CSharp/Contests2020/CFR616/A.cs
@@ -6,14 +6,26 @@ class A
 	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 
-	static long Solve()
+	static string Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var h = Read();
-		//int n = h[0], m = h[1];
-		var s = Console.ReadLine();
+		var s = Console.ReadLine().Select(c => c - '0').ToList();
 
-		var r = 0L;
-		return r;
+		while (s.Any() && s.Last() % 2 == 0)
+			s.RemoveAt(s.Count - 1);
+		if (!s.Any()) return "-1";
+
+		if (s.Sum() % 2 == 0) return string.Join("", s);
+
+		while (s.Any())
+		{
+			if (s[0] % 2 == 0) s.RemoveAt(0);
+			else { s.RemoveAt(0); break; }
+		}
+		while (s.Any() && s[0] == 0)
+			s.RemoveAt(0);
+
+		if (!s.Any()) return "-1";
+		return string.Join("", s);
 	}
 }
--- CSharp/Contests2020/CFR616/C.cs
+++ CSharp/Contests2020/CFR616/C.cs
@@ -8,12 +8,34 @@ class C
 
 	static long Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
 		var h = Read();
-		//int n = h[0], m = h[1];
-		var s = Console.ReadLine();
+		int n = h[0], m = h[1], k = Math.Min(h[2], m - 1), k2 = m - 1 - k;
+		var a = Read();
 
-		var r = 0L;
-		return r;
+		return Last(0, 1 << 30, x =>
+		{
+			var b = Array.ConvertAll(a, v => v >= x);
+
+			for (int i = 0; i <= k; i++)
+			{
+				int l1 = i, r1 = n - 1 - k + i;
+
+				var ok = true;
+				for (int j = 0; j <= k2; j++)
+				{
+					int l2 = l1 + j, r2 = r1 - k2 + j;
+					if (!b[l2] && !b[r2]) { ok = false; break; }
+				}
+				if (ok) return true;
+			}
+			return false;
+		});
+	}
+
+	static int Last(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
 	}
 }
