1fe2a350ec4457f7a6af836d2790b4c98d07e020
Keiho Sakapon <git@saka-pon.net>
2020-02-12 08:35:30 +0900

    Other AC

---
+++ CSharp/Contests2020/CFR613/B2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class B2
+{
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+
+	static string Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+
+		var s1 = new long[n + 1];
+		for (int i = 0; i < n; i++)
+			if ((s1[i + 1] = s1[i] + a[i]) <= 0) return "NO";
+
+		var s2 = new long[n + 1];
+		for (int i = n - 1; i >= 0; i--)
+			if ((s2[i] = s2[i + 1] + a[i]) <= 0) return "NO";
+
+		return "YES";
+	}
+}
