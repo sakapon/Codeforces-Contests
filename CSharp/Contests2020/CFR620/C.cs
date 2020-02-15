293ad3fd10eb792871eac328e63b16834c6de1e3
Keiho Sakapon <git@saka-pon.net>
2020-02-15 11:16:29 +0900

    Initialize

---
+++ CSharp/Contests2020/CFR620/C.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class C
+{
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+
+	static long Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var h = Read();
+		//int n = h[0], m = h[1];
+		var s = Console.ReadLine();
+
+		var r = 0L;
+		return r;
+	}
+}
