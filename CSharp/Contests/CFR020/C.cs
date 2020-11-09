9c2f4b07480f8b6262f25c99467013c481a3402b
Keiho Sakapon <git@saka-pon.net>
2020-11-09 22:22:10 +0900

    Update library

--- CSharp/Contests/CFR020/C.cs
+++ CSharp/Contests/CFR020/C.cs
@@ -1,83 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class C
 {
-	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static void Main()
 	{
 		var h = Read();
-		var n = h[0];
-		var es = new int[h[1]].Select(_ => Read()).ToArray();
+		int n = h[0], m = h[1];
+		var es = Array.ConvertAll(new bool[m], _ => Read());
 
-		var r = Dijkstra(n, 1, n, es);
-		Console.WriteLine(r == null ? "-1" : string.Join(" ", r));
+		var (c, inEdges) = Dijkstra(n + 1, es, false, 1, n);
+		if (inEdges[n] == null) { Console.WriteLine(-1); return; }
+
+		var path = GetPathVertexes(inEdges, n);
+		Console.WriteLine(string.Join(" ", path));
 	}
 
-	static int[] Dijkstra(int n, int sv, int ev, int[][] es)
+	static Tuple<long[], int[][]> Dijkstra(int n, int[][] es, bool directed, int sv, int ev = 1)
 	{
-		var map = Array.ConvertAll(new int[n + 1], _ => new List<int[]>());
+		var map = Array.ConvertAll(new bool[n], _ => new List<int[]>());
 		foreach (var e in es)
 		{
-			map[e[0]].Add(new[] { e[1], e[2] });
-			map[e[1]].Add(new[] { e[0], e[2] });
+			map[e[0]].Add(new[] { e[0], e[1], e[2] });
+			if (!directed) map[e[1]].Add(new[] { e[1], e[0], e[2] });
 		}
 
-		var from = Enumerable.Repeat(-1, n + 1).ToArray();
-		var u = Enumerable.Repeat(long.MaxValue, n + 1).ToArray();
-		var pq = PQ<int>.Create(v => u[v]);
-		u[sv] = 0;
-		pq.Push(sv);
+		var cs = Array.ConvertAll(new bool[n], _ => long.MaxValue);
+		var inEdges = new int[n][];
+		var q = PQ<int>.CreateWithKey(v => cs[v]);
+		cs[sv] = 0;
+		q.Push(sv);
 
-		while (pq.Count > 0)
+		while (q.Count > 0)
 		{
-			var v = pq.Pop();
+			var vc = q.Pop();
+			var v = vc.Value;
 			if (v == ev) break;
+			if (cs[v] < vc.Key) continue;
+
 			foreach (var e in map[v])
 			{
-				if (u[e[0]] <= u[v] + e[1]) continue;
-				from[e[0]] = v;
-				u[e[0]] = u[v] + e[1];
-				pq.Push(e[0]);
+				if (cs[e[1]] <= cs[v] + e[2]) continue;
+				cs[e[1]] = cs[v] + e[2];
+				inEdges[e[1]] = e;
+				q.Push(e[1]);
 			}
 		}
+		return Tuple.Create(cs, inEdges);
+	}
 
-		if (from[ev] == -1) return null;
-		var path = new List<int>();
-		for (var v = ev; v != -1; v = from[v])
-			path.Add(v);
-		path.Reverse();
+	static int[] GetPathVertexes(int[][] inEdges, int ev)
+	{
+		var path = new Stack<int>();
+		path.Push(ev);
+		for (var e = inEdges[ev]; e != null; e = inEdges[e[0]])
+			path.Push(e[0]);
 		return path.ToArray();
 	}
 }
 
 class PQ<T> : List<T>
 {
-	public static PQ<T> Create(T[] vs = null, bool desc = false)
+	public static PQ<T> Create(bool desc = false)
 	{
 		var c = Comparer<T>.Default;
 		return desc ?
-			new PQ<T>(vs, (x, y) => c.Compare(y, x)) :
-			new PQ<T>(vs, c.Compare);
+			new PQ<T>((x, y) => c.Compare(y, x)) :
+			new PQ<T>(c.Compare);
 	}
 
-	public static PQ<T> Create<TKey>(Func<T, TKey> getKey, T[] vs = null, bool desc = false)
+	public static PQ<T> Create<TKey>(Func<T, TKey> toKey, bool desc = false)
 	{
 		var c = Comparer<TKey>.Default;
 		return desc ?
-			new PQ<T>(vs, (x, y) => c.Compare(getKey(y), getKey(x))) :
-			new PQ<T>(vs, (x, y) => c.Compare(getKey(x), getKey(y)));
+			new PQ<T>((x, y) => c.Compare(toKey(y), toKey(x))) :
+			new PQ<T>((x, y) => c.Compare(toKey(x), toKey(y)));
+	}
+
+	public static PQ<T, TKey> CreateWithKey<TKey>(Func<T, TKey> toKey, bool desc = false)
+	{
+		var c = Comparer<TKey>.Default;
+		return desc ?
+			new PQ<T, TKey>(toKey, (x, y) => c.Compare(y.Key, x.Key)) :
+			new PQ<T, TKey>(toKey, (x, y) => c.Compare(x.Key, y.Key));
 	}
 
 	Comparison<T> c;
 	public T First => this[0];
-
-	PQ(T[] vs, Comparison<T> _c)
-	{
-		c = _c;
-		if (vs != null) foreach (var v in vs) Push(v);
-	}
+	internal PQ(Comparison<T> _c) { c = _c; }
 
 	void Swap(int i, int j) { var o = this[i]; this[i] = this[j]; this[j] = o; }
 	void UpHeap(int i) { for (int j; i > 0 && c(this[j = (i - 1) / 2], this[i]) > 0; Swap(i, i = j)) ; }
@@ -95,6 +106,8 @@ class PQ<T> : List<T>
 		Add(v);
 		UpHeap(Count - 1);
 	}
+	public void PushRange(T[] vs) { foreach (var v in vs) Push(v); }
+
 	public T Pop()
 	{
 		var r = this[0];
@@ -103,4 +116,13 @@ class PQ<T> : List<T>
 		DownHeap(0);
 		return r;
 	}
+}
+
+class PQ<T, TKey> : PQ<KeyValuePair<TKey, T>>
+{
+	Func<T, TKey> ToKey;
+	internal PQ(Func<T, TKey> toKey, Comparison<KeyValuePair<TKey, T>> c) : base(c) { ToKey = toKey; }
+
+	public void Push(T v) => Push(new KeyValuePair<TKey, T>(ToKey(v), v));
+	public void PushRange(T[] vs) { foreach (var v in vs) Push(v); }
 }
