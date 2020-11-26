f807d6266aa1785f7d6748523a4201cd48c2bc38
Keiho Sakapon <git@saka-pon.net>
2020-11-26 18:15:10 +0900

    Improve code

--- CSharp/Contests2020/CFR677/C.cs
+++ CSharp/Contests2020/CFR677/C.cs
@@ -4,7 +4,6 @@ using System.Linq;
 class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static long Solve()
 	{
@@ -13,15 +12,9 @@ class C
 
 		if (a.Distinct().Count() == 1) return -1;
 
-		var oa = a.Select((x, i) => (x, i)).OrderBy(v => -v.x).ToArray();
-
-		foreach (var (x, i) in oa)
-		{
-			if (i > 0 && a[i - 1] < a[i] || i + 1 < n && a[i] > a[i + 1])
-			{
+		foreach (var i in Enumerable.Range(0, n).OrderBy(i => -a[i]))
+			if (i - 1 >= 0 && a[i - 1] < a[i] || i + 1 < n && a[i] > a[i + 1])
 				return i + 1;
-			}
-		}
 		return -1;
 	}
 }
