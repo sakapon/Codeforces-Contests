32a7c2576609ba9485c568b9169ca81af827777b
Keiho Sakapon <git@saka-pon.net>
2021-01-04 10:31:01 +0900

    AC

--- CSharp/Contests2020/CFR686/E.cs
+++ CSharp/Contests2020/CFR686/E.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class E
@@ -6,15 +7,37 @@ class E
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main()
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static long Solve()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var (n, m) = Read2();
-		var n = int.Parse(Console.ReadLine());
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var n = long.Parse(Console.ReadLine());
+		var es = Array.ConvertAll(new bool[n], _ => Read());
 
-		Console.WriteLine(string.Join(" ", a));
+		var map = Array.ConvertAll(new bool[n + 1], _ => new HashSet<int>());
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e[1]);
+			map[e[1]].Add(e[0]);
+		}
+
+		var counts = Array.ConvertAll(new bool[n + 1], _ => 1L);
+		var q = new Queue<int>();
+
+		for (int v = 1; v <= n; v++)
+		{
+			if (map[v].Count == 1) q.Enqueue(v);
+		}
+
+		while (q.TryDequeue(out var v))
+		{
+			var nv = map[v].First();
+			map[nv].Remove(v);
+			counts[nv] += counts[v];
+			if (map[nv].Count == 1) q.Enqueue(nv);
+		}
+
+		// Either.
+		return n * (n - 1) - Enumerable.Range(1, (int)n).Where(v => map[v].Count == 2).Select(v => counts[v]).Sum(c => c * (c - 1) / 2);
+		//return Enumerable.Range(1, (int)n).Where(v => map[v].Count == 2).Select(v => counts[v]).Sum(c => c * (c - 1) / 2 + c * (n - c));
 	}
 }
