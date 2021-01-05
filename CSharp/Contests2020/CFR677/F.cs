6116df39c57245a88e0069f00955927bcae77508
Keiho Sakapon <git@saka-pon.net>
2021-01-05 19:52:15 +0900

    Update code

--- CSharp/Contests2020/CFR677/F.cs
+++ CSharp/Contests2020/CFR677/F.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 class F
 {
@@ -10,48 +9,56 @@ class F
 		var (n, m, k) = Read3();
 		var a = Array.ConvertAll(new bool[n], _ => Read());
 
-		var max = NewArray3(n, k, m / 2 + 1, int.MinValue);
-		var dp = NewArray2(n + 1, k, int.MinValue);
-		dp[0][0] = 0;
+		// row, count, remainder
+		var dp = NewArray3(n, m / 2 + 1, k, -1);
 
 		for (int i = 0; i < n; i++)
 		{
-			max[i][0][0] = 0;
-			for (int aj = 0; aj < m; aj++)
+			dp[i][0][0] = 0;
+			for (int j = 0; j < m; j++)
 			{
 				for (int c = m / 2 - 1; c >= 0; c--)
 				{
 					for (int r = 0; r < k; r++)
 					{
-						if (max[i][r][c] < 0) continue;
-						var nv = max[i][r][c] + a[i][aj];
-						max[i][nv % k][c + 1] = Math.Max(max[i][nv % k][c + 1], nv);
+						if (dp[i][c][r] < 0) continue;
+						var nv = dp[i][c][r] + a[i][j];
+						dp[i][c + 1][nv % k] = Math.Max(dp[i][c + 1][nv % k], nv);
 					}
 				}
 			}
-			for (int j = 0; j < k; j++)
-			{
-				max[i][j][0] = max[i][j].Max();
-			}
-		}
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < k; j++)
+			for (int c = m / 2; c > 0; c--)
 			{
-				for (int l = 0; l < k; l++)
+				for (int r = 0; r < k; r++)
 				{
-					if (dp[i][j] < 0 || max[i][l][0] < 0) continue;
-					var nk = (j + l) % k;
-					dp[i + 1][nk] = Math.Max(dp[i + 1][nk], dp[i][j] + max[i][l][0]);
+					dp[i][0][r] = Math.Max(dp[i][0][r], dp[i][c][r]);
 				}
 			}
 		}
 
-		Console.WriteLine(dp[n][0]);
+		var sum = NewArray1(k, -1);
+		sum[0] = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			var t = NewArray1(k, -1);
+
+			for (int r = 0; r < k; r++)
+			{
+				for (int ir = 0; ir < k; ir++)
+				{
+					if (sum[r] < 0 || dp[i][0][ir] < 0) continue;
+					var nv = sum[r] + dp[i][0][ir];
+					t[nv % k] = Math.Max(t[nv % k], nv);
+				}
+			}
+			sum = t;
+		}
+
+		Console.WriteLine(sum[0]);
 	}
 
 	static T[] NewArray1<T>(int n, T v = default) => Array.ConvertAll(new bool[n], _ => v);
-	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 	static T[][][] NewArray3<T>(int n1, int n2, int n3, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => Array.ConvertAll(new bool[n3], ___ => v)));
 }
