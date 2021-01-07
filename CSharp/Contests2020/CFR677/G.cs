67c4ad003917c9dbdad459de9a4eb715c4883fe8
Keiho Sakapon <git@saka-pon.net>
2021-01-07 23:16:54 +0900

    AC

---
+++ CSharp/Contests2020/CFR677/G.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.Graphs0.Spp1;
+
+class G
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static Edge[] ReadEdges(int n) => Array.ConvertAll(new bool[n], _ => new Edge(Read()));
+	static void Main()
+	{
+		var (n, m, k) = Read3();
+		var es = ReadEdges(m);
+		var rs = ReadEdges(k);
+
+		var map = GraphConvert1.WeightedEdgesToMap(n + 1, es, false);
+		var costMap = Enumerable.Range(0, n + 1).Select(sv => ShortestPathCore1.Dijkstra(n + 1, v => map[v].ToArray(), sv)).ToArray();
+
+		long CostSum(Edge e) => rs.Sum(r =>
+		{
+			var (a, b) = (r.From, r.To);
+			var m = costMap[a][b];
+			m = Math.Min(m, costMap[a][e.From] + costMap[e.To][b]);
+			m = Math.Min(m, costMap[a][e.To] + costMap[e.From][b]);
+			return m;
+		});
+		Console.WriteLine(es.Min(CostSum));
+	}
+}
+
+namespace AlgorithmLab.Graphs0.Spp1
+{
+	public static class ShortestPathCore1
+	{
+		public static long[] Dijkstra(int vertexesCount, Func<int, Edge[]> getNextEdges, int startVertexId, int endVertexId = -1)
+		{
+			var costs = Array.ConvertAll(new bool[vertexesCount], _ => long.MaxValue);
+			var q = PriorityQueue<int>.CreateWithKey(v => costs[v]);
+			costs[startVertexId] = 0;
+			q.Push(startVertexId);
+
+			while (q.Any)
+			{
+				var (v, c) = q.Pop();
+				if (v == endVertexId) break;
+				if (costs[v] < c) continue;
+
+				// IEnumerable<T>, List<T>, T[] の順に高速になります。
+				foreach (var e in getNextEdges(v))
+				{
+					var (nv, nc) = (e.To, c + e.Cost);
+					if (costs[nv] <= nc) continue;
+					costs[nv] = nc;
+					q.Push(nv);
+				}
+			}
+			return costs;
+		}
+	}
+
+	public struct Edge
+	{
+		public static Edge Invalid { get; } = new Edge(-1, -1, -1);
+
+		public int From { get; }
+		public int To { get; }
+		public long Cost { get; }
+
+		public Edge(int from, int to, long cost)
+		{
+			From = from;
+			To = to;
+			Cost = cost;
+		}
+		public Edge(int[] e) : this(e[0], e[1], e.Length > 2 ? e[2] : 0) { }
+		public Edge(long[] e) : this((int)e[0], (int)e[1], e.Length > 2 ? e[2] : 0) { }
+
+		public Edge Reverse() => new Edge(To, From, Cost);
+	}
+
+	public static class GraphConvert1
+	{
+		public static List<int>[] UnweightedEdgesToMap(int vertexesCount, int[][] edges, bool directed)
+		{
+			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<int>());
+			foreach (var e in edges)
+			{
+				// 入力チェックは省略。
+				map[e[0]].Add(e[1]);
+				if (!directed) map[e[1]].Add(e[0]);
+			}
+			return map;
+		}
+
+		public static List<Edge>[] WeightedEdgesToMap(int vertexesCount, Edge[] edges, bool directed)
+		{
+			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<Edge>());
+			foreach (var e in edges)
+			{
+				// 入力チェックは省略。
+				map[e.From].Add(e);
+				if (!directed) map[e.To].Add(e.Reverse());
+			}
+			return map;
+		}
+
+		public static List<Edge>[] WeightedEdgesToMap(int vertexesCount, int[][] edges, bool directed)
+		{
+			return WeightedEdgesToMap(vertexesCount, Array.ConvertAll(edges, e => new Edge(e)), directed);
+		}
+	}
+
+	public class PriorityQueue<T>
+	{
+		public static PriorityQueue<T> Create(bool descending = false)
+		{
+			var c = Comparer<T>.Default;
+			return descending ?
+				new PriorityQueue<T>((x, y) => c.Compare(y, x)) :
+				new PriorityQueue<T>(c.Compare);
+		}
+
+		public static PriorityQueue<T> Create<TKey>(Func<T, TKey> keySelector, bool descending = false)
+		{
+			if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+
+			var c = Comparer<TKey>.Default;
+			return descending ?
+				new PriorityQueue<T>((x, y) => c.Compare(keySelector(y), keySelector(x))) :
+				new PriorityQueue<T>((x, y) => c.Compare(keySelector(x), keySelector(y)));
+		}
+
+		public static PriorityQueue<T, TKey> CreateWithKey<TKey>(Func<T, TKey> keySelector, bool descending = false)
+		{
+			var c = Comparer<TKey>.Default;
+			return descending ?
+				new PriorityQueue<T, TKey>(keySelector, (x, y) => c.Compare(y.key, x.key)) :
+				new PriorityQueue<T, TKey>(keySelector, (x, y) => c.Compare(x.key, y.key));
+		}
+
+		List<T> l = new List<T> { default };
+		Comparison<T> c;
+
+		public T First
+		{
+			get
+			{
+				if (l.Count <= 1) throw new InvalidOperationException("The heap is empty.");
+				return l[1];
+			}
+		}
+
+		public int Count => l.Count - 1;
+		public bool Any => l.Count > 1;
+
+		internal PriorityQueue(Comparison<T> comparison)
+		{
+			c = comparison ?? throw new ArgumentNullException(nameof(comparison));
+		}
+
+		// x の親: x/2
+		// x の子: 2x, 2x+1
+		void UpHeap(int i)
+		{
+			for (int j; (j = i >> 1) > 0 && c(l[j], l[i]) > 0; i = j)
+				(l[i], l[j]) = (l[j], l[i]);
+		}
+
+		void DownHeap(int i)
+		{
+			for (int j; (j = i << 1) < l.Count; i = j)
+			{
+				if (j + 1 < l.Count && c(l[j], l[j + 1]) > 0) j++;
+				if (c(l[i], l[j]) > 0) (l[i], l[j]) = (l[j], l[i]); else break;
+			}
+		}
+
+		public void Push(T value)
+		{
+			l.Add(value);
+			UpHeap(l.Count - 1);
+		}
+
+		public void PushRange(IEnumerable<T> values)
+		{
+			if (values != null) foreach (var v in values) Push(v);
+		}
+
+		public T Pop()
+		{
+			if (l.Count <= 1) throw new InvalidOperationException("The heap is empty.");
+
+			var r = l[1];
+			l[1] = l[l.Count - 1];
+			l.RemoveAt(l.Count - 1);
+			DownHeap(1);
+			return r;
+		}
+	}
+
+	// キーをキャッシュすることにより、キーが不変であることを保証します。
+	public class PriorityQueue<T, TKey> : PriorityQueue<(T value, TKey key)>
+	{
+		Func<T, TKey> KeySelector;
+
+		internal PriorityQueue(Func<T, TKey> keySelector, Comparison<(T value, TKey key)> comparison) : base(comparison)
+		{
+			KeySelector = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+		}
+
+		public void Push(T value)
+		{
+			Push((value, KeySelector(value)));
+		}
+
+		public void PushRange(IEnumerable<T> values)
+		{
+			if (values != null) foreach (var v in values) Push(v);
+		}
+	}
+}
