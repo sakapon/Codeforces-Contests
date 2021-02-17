fa5658f953c3b57084322591237c87be6775b4fe
Keiho Sakapon <git@saka-pon.net>
2021-02-17 21:42:26 +0900

    AC

--- CSharp/Contests2020/CFGR012/B.cs
+++ CSharp/Contests2020/CFGR012/B.cs
@@ -5,18 +5,22 @@ class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static long Solve()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var (n, m) = Read2();
-		var n = int.Parse(Console.ReadLine());
-		var a = Read();
+		var (n, k) = Read2();
 		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		var r = 0L;
-		return r;
+		for (int i = 0; i < n; i++)
+		{
+			var ok = true;
+			for (int j = 0; j < n; j++)
+				if (NormL1(Subtract(ps[i], ps[j])) > k) { ok = false; break; }
+			if (ok) return 1;
+		}
+		return -1;
 	}
+
+	static int[] Subtract(int[] v1, int[] v2) => new[] { v1[0] - v2[0], v1[1] - v2[1] };
+	static int NormL1(int[] v) => Math.Abs(v[0]) + Math.Abs(v[1]);
 }
--- CSharp/Contests2020/CFGR012/D.cs
+++ CSharp/Contests2020/CFGR012/D.cs
@@ -4,17 +4,23 @@ using System.Linq;
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main()
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static string Solve()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var (n, m) = Read2();
 		var n = int.Parse(Console.ReadLine());
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		Console.WriteLine(string.Join(" ", a));
+		var d = a.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+		var k1 = d.Count == n ? "1" : "0";
+
+		int l = 0, r = 1;
+		for (int i = 1; i < n; i++)
+		{
+			if (!d.ContainsKey(i)) return new string('0', n + 1 - i) + new string('1', i - 1);
+			if (a[l] != i && a[^r] != i || d[i] > 1) return k1 + new string('0', n - 1 - i) + new string('1', i);
+			if (a[l] == i) l++;
+			else r++;
+		}
+		return new string('1', n);
 	}
 }
