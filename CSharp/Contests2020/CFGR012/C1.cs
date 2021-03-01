d8c52d7fef93014985ffe8217aa84d1851c111d0
Keiho Sakapon <git@saka-pon.net>
2021-03-01 23:32:11 +0900

    AC

---
+++ CSharp/Contests2020/CFGR012/C1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class C1
+{
+	const char X = 'X', O = 'O';
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static string Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var s = Array.ConvertAll(new bool[n], _ => Console.ReadLine().ToCharArray());
+
+		var c = new int[3];
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				if (s[i][j] == X)
+					c[(i + j) % 3]++;
+
+		var m = Enumerable.Range(0, 3).OrderBy(x => c[x]).First();
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				if (s[i][j] == X && (i + j) % 3 == m)
+					s[i][j] = O;
+
+		return string.Join("\n", s.Select(cs => new string(cs)));
+	}
+}
