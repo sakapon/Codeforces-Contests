be8d8817ba88ae34765361c11578307a98d9a188
Keiho Sakapon <git@saka-pon.net>
2021-03-02 09:52:58 +0900

    Improve code

--- CSharp/Contests2020/CFGR012/C2.cs
+++ CSharp/Contests2020/CFGR012/C2.cs
@@ -20,22 +20,13 @@ class C2
 				else if (s[i][j] == O)
 					co[(i + j) % 3]++;
 
-		var k = cx.Sum() + co.Sum();
-		var (mx, mo) = ArgMin();
-		(int, int) ArgMin()
-		{
-			for (int i = 0; i < 3; i++)
-				for (int j = 0; j < 3; j++)
-					if (i != j && cx[i] + co[j] <= k / 3)
-						return (i, j);
-			throw new InvalidOperationException();
-		}
+		var mx = Enumerable.Range(0, 3).OrderBy(x => cx[x] + co[(x + 1) % 3]).First();
 
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < n; j++)
 				if (s[i][j] == X && (i + j) % 3 == mx)
 					s[i][j] = O;
-				else if (s[i][j] == O && (i + j) % 3 == mo)
+				else if (s[i][j] == O && (i + j) % 3 == (mx + 1) % 3)
 					s[i][j] = X;
 
 		return string.Join("\n", s.Select(cs => new string(cs)));
