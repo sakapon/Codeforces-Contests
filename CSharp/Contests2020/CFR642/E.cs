16ae4079e51c42df899ff0e81a163a30effa7632
Keiho Sakapon <git@saka-pon.net>
2021-03-03 23:55:32 +0900

    AC

--- CSharp/Contests2020/CFR642/E.cs
+++ CSharp/Contests2020/CFR642/E.cs
@@ -3,16 +3,39 @@ using System.Linq;
 
 class E
 {
-	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
-	static void Main()
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static long Solve()
 	{
-		Console.ReadLine();
+		var (n, k) = Read2();
 		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = Read();
-		var ps = new int[h[0]].Select(_ => Read()).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var all = s.Count(c => c == '1');
+		var min = all;
+
+		for (int j = 0; j < k; j++)
+		{
+			// オン区間の前・中・後
+			var (c1, c2, c3) = (0, 0, 0);
+			var count = 0;
+
+			for (int i = j; i < n; i += k)
+			{
+				if (s[i] == '1')
+				{
+					count++;
+					(c2, c3) = (Math.Min(c1, c2), Math.Min(c2, c3) + 1);
+					c1++;
+				}
+				else
+				{
+					(c2, c3) = (Math.Min(c1, c2) + 1, Math.Min(c2, c3));
+				}
+			}
+			var target = Math.Min(c1, Math.Min(c2, c3));
+			min = Math.Min(min, target + all - count);
+		}
+		return min;
 	}
 }
