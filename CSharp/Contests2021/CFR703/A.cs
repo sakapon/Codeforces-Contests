4a89d1d02eba3409220580582b662490868bc021
Keiho Sakapon <git@saka-pon.net>
2021-03-05 19:52:47 +0900

    AC

--- CSharp/Contests2021/CFR703/A.cs
+++ CSharp/Contests2021/CFR703/A.cs
@@ -4,19 +4,19 @@ using System.Linq;
 class A
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
-	static long Solve()
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
+	static bool Solve()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var (n, m) = Read2();
 		var n = int.Parse(Console.ReadLine());
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var h = Read();
 
-		var r = 0L;
-		return r;
+		long s = 0, t = 0;
+		for (int i = 0; i < n; i++)
+		{
+			s += h[i];
+			t += i;
+			if (s < t) return false;
+		}
+		return true;
 	}
 }
--- CSharp/Contests2021/CFR703/B.cs
+++ CSharp/Contests2021/CFR703/B.cs
@@ -3,20 +3,23 @@ using System.Linq;
 
 class B
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long x, long y) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
-	static long Solve()
+	static object Solve()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var (n, m) = Read2();
 		var n = int.Parse(Console.ReadLine());
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var ps = Array.ConvertAll(new bool[n], _ => Read2L());
 
-		var r = 0L;
-		return r;
+		var xs = Array.ConvertAll(ps, p => p.x);
+		var ys = Array.ConvertAll(ps, p => p.y);
+		Array.Sort(xs);
+		Array.Sort(ys);
+
+		var l = (n - 1) / 2;
+		var r = n / 2;
+		var (d, u) = (l, r);
+
+		return (xs[r] - xs[l] + 1) * (ys[u] - ys[d] + 1);
 	}
 }
