43c8134fde278ddc61b16d5b8174808a65edd859
Keiho Sakapon <git@saka-pon.net>
2021-03-08 08:51:06 +0900

    Improve code

---
+++ CSharp/Contests2021/CFR703/A2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class A2
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
+	static bool Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var h = ReadL();
+
+		for (int i = 0; i < n - 1; i++)
+			if ((h[i + 1] += h[i] - i) < i + 1) return false;
+		return true;
+	}
+}
--- CSharp/Contests2021/CFR705/D.cs
+++ CSharp/Contests2021/CFR705/D.cs
@@ -1,93 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static (long i, long x) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main()
 	{
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
 		var (n, qc) = Read2();
-		var a = ReadL();
-		var qs = Array.ConvertAll(new bool[qc], _ => Read2L());
+		var a = Read();
+		var qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
-		var qfs = Array.ConvertAll(qs, q => Factorize(q.x));
-
-		var fmaps = Array.ConvertAll(a, v =>
-		{
-			var map = new Map<long, int>();
-			foreach (var p in Factorize(v))
-				map[p]++;
-			return map;
-		});
-		// final state
-		for (int k = 0; k < qc; k++)
-		{
-			var (i, x) = qs[k];
-			var map = fmaps[i - 1];
-			foreach (var p in qfs[k])
-				map[p]++;
-		}
-
-		// final GCD
-		var gcdMap = new Map<long, int>();
-		foreach (var (k, v) in fmaps[0])
-			gcdMap[k] = v;
-		for (int i = 1; i < n; i++)
-		{
-			var keys = gcdMap.Keys.ToArray();
-			foreach (var k in keys)
-			{
-				var v = Math.Min(gcdMap[k], fmaps[i][k]);
-				if (v > 0) gcdMap[k] = v;
-				else gcdMap.Remove(k);
-			}
-		}
+		var pmaps = Array.ConvertAll(a, v => new Map<long, int>());
+		var counts = new Map<(long, int), int>();
 		var gcd = 1L;
-		foreach (var (k, v) in gcdMap)
-		{
-			gcd *= MPow(k, v);
-			gcd %= M;
-		}
 
-		var r = new long[qc];
-		r[^1] = gcd;
-		for (int k = qc - 1; k > 0; k--)
+		void Multiply(long i, long x)
 		{
-			var (i, x) = qs[k];
-			var map = fmaps[i - 1];
-
-			var nv = r[k];
-			foreach (var p in qfs[k])
+			foreach (var p in Factorize(x))
 			{
-				map[p]--;
-				if (map[p] < gcdMap[p])
-				{
-					gcdMap[p]--;
-					nv *= MInv(p);
-					nv %= M;
-				}
+				var t = (p, ++pmaps[i][p]);
+				if (++counts[t] == n)
+					gcd = gcd * p % M;
 			}
-			r[k - 1] = nv;
 		}
 
-		Console.WriteLine(string.Join("\n", r));
+		for (int i = 0; i < n; i++)
+		{
+			Multiply(i, a[i]);
+		}
+		foreach (var (i, x) in qs)
+		{
+			Multiply(i - 1, x);
+			Console.WriteLine(gcd);
+		}
+		Console.Out.Flush();
 	}
 
 	const long M = 1000000007;
-	static long MPow(long b, long i)
-	{
-		long r = 1;
-		for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
-		return r;
-	}
-	static long MInv(long x) => MPow(x, M - 2);
-
-	static long Gcd(long a, long b) { for (long r; (r = a % b) > 0; a = b, b = r) ; return b; }
-	static long Lcm(long a, long b) => a / Gcd(a, b) * b;
 
 	static long[] Factorize(long n)
 	{
