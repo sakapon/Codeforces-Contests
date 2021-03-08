f26d295024775c93ec29826d1e212abf71751c8c
Keiho Sakapon <git@saka-pon.net>
2021-03-08 16:06:42 +0900

    AC

--- CSharp/Contests2021/CFR703/D.cs
+++ CSharp/Contests2021/CFR703/D.cs
@@ -5,16 +5,32 @@ class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var (n, m) = Read2();
-		var n = int.Parse(Console.ReadLine());
+		var (n, k) = Read2();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		Console.WriteLine(string.Join(" ", a));
+		Console.WriteLine(Last(1, n, x =>
+		{
+			var b = Array.ConvertAll(a, v => v < x ? -1 : 1);
+
+			var s = new int[n + 1];
+			for (int i = 0; i < n; ++i) s[i + 1] = s[i] + b[i];
+
+			var min = 0;
+			for (int i = k; i <= n; i++)
+			{
+				min = Math.Min(min, s[i - k]);
+				if (min < s[i]) return true;
+			}
+			return false;
+		}));
+	}
+
+	static int Last(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
 	}
 }
