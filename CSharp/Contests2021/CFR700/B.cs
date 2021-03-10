7309c186e38cc2eb9c88aaef7a9b42cfdeab782f
Keiho Sakapon <git@saka-pon.net>
2021-03-10 12:16:13 +0900

    Improve code

--- CSharp/Contests2021/CFR700/B.cs
+++ CSharp/Contests2021/CFR700/B.cs
@@ -3,10 +3,7 @@ using System.Linq;
 
 class B
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static (long, long, long) Read3L() { var a = ReadL(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
 	static bool Solve()
--- CSharp/Contests2021/CFR700/C.cs
+++ CSharp/Contests2021/CFR700/C.cs
@@ -11,10 +11,9 @@ class C
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		if (n == 1) return 1;
 
 		var (l, m, r) = (0, (n + 1) / 2, n + 1);
-		var (al, am, ar) = (1 << 30, Query(m), 1 << 30);
+		var am = Query(m);
 		int t;
 
 		// Keep a[l] > a[m] < a[r].
@@ -23,31 +22,29 @@ class C
 			if (m - l >= r - m)
 			{
 				// left-side
-				var v = Query(t = l + (m - l) / 2);
-				if (v < am)
+				var v = Query(t = m - (m - l) / 2);
+				if (v <= am)
 				{
 					(m, r) = (t, m);
-					(am, ar) = (v, am);
+					am = v;
 				}
 				else
 				{
 					l = t;
-					al = v;
 				}
 			}
 			else
 			{
 				// right-side
 				var v = Query(t = m + (r - m) / 2);
-				if (v < am)
+				if (v <= am)
 				{
 					(m, l) = (t, m);
-					(am, al) = (v, am);
+					am = v;
 				}
 				else
 				{
 					r = t;
-					ar = v;
 				}
 			}
 		}
