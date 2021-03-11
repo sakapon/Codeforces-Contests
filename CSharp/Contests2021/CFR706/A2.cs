615d8f2371ad8d73a645b12cde9a6e9384747d5e
Keiho Sakapon <git@saka-pon.net>
2021-03-11 21:07:44 +0900

    Improve code

---
+++ CSharp/Contests2021/CFR706/A2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class A2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
+	static bool Solve()
+	{
+		var (n, k) = Read2();
+		var s = Console.ReadLine();
+
+		var c = Enumerable.Range(0, n / 2).TakeWhile(i => s[i] == s[^(i + 1)]).Count();
+		if (c < k) return false;
+		return n >= 2 * k + 1;
+	}
+}
--- CSharp/Contests2021/CFR706/C.cs
+++ CSharp/Contests2021/CFR706/C.cs
@@ -3,12 +3,10 @@ using System.Linq;
 
 class C
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int x, int y) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static (long x, long y) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
-	static double Solve()
+	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
 		var ps = Array.ConvertAll(new bool[2 * n], _ => Read2L());
@@ -17,8 +15,7 @@ class C
 		var ys = ps.Where(p => p.x == 0).Select(p => Math.Abs(p.y)).ToArray();
 		Array.Sort(xs);
 		Array.Sort(ys);
-		//Array.Reverse(ys);
 
-		return Enumerable.Range(0, n).Sum(i => Math.Sqrt(xs[i] * xs[i] + ys[i] * ys[i]));
+		return xs.Zip(ys, (x, y) => Math.Sqrt(x * x + y * y)).Sum();
 	}
 }
