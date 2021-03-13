9f2017e809a2101b12ec4d53e2f5ea0d1836c9af
Keiho Sakapon <git@saka-pon.net>
2021-03-13 15:01:14 +0900

    Update templates

--- CSharp/Contests2021/CFR000/C.cs
+++ CSharp/Contests2021/CFR000/C.cs
@@ -6,17 +6,18 @@ class C
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	//static void Main() => Console.WriteLine(Solve());
+	//static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
-	static long Solve()
+	static object Solve()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var (n, m) = Read2();
 		var n = int.Parse(Console.ReadLine());
+		var (n2, m) = Read2();
+		var s = Console.ReadLine();
 		var a = Read();
 		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		var r = 0L;
-		return r;
+		if (n == 0) return "NO";
+		return "YES\n" + string.Join(" ", a);
 	}
 }
--- CSharp/Contests2021/CFR000/D.cs
+++ CSharp/Contests2021/CFR000/D.cs
@@ -6,15 +6,15 @@ class D
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main()
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var (n, m) = Read2();
 		var n = int.Parse(Console.ReadLine());
+		var (n2, m) = Read2();
+		var s = Console.ReadLine();
 		var a = Read();
 		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		Console.WriteLine(string.Join(" ", a));
+		return string.Join(" ", a);
 	}
 }
