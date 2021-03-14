54596aafdc85eb35c4a1bf3e44584bce8e2d7f5a
Keiho Sakapon <git@saka-pon.net>
2021-03-14 13:27:03 +0900

    Improve code

--- CSharp/Contests2021/CFR707/B.cs
+++ CSharp/Contests2021/CFR707/B.cs
@@ -4,10 +4,6 @@ using System.Linq;
 class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	//static void Main() => Console.WriteLine(Solve());
-	//static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
@@ -16,13 +12,9 @@ class B
 
 		var raq = new StaticRAQ(n);
 		for (int i = 0; i < n; i++)
-		{
-			if (a[i] > 0)
-				raq.Add(i + 1 - a[i], i + 1, 1);
-		}
+			raq.Add(i + 1 - a[i], i + 1, 1);
 
-		var r = raq.GetAll();
-		return string.Join(" ", r.Select(x => x > 0 ? 1 : 0));
+		return string.Join(" ", raq.GetAll().Select(Math.Sign));
 	}
 }
 
@@ -35,6 +27,7 @@ class StaticRAQ
 	// 範囲外のインデックスも可。
 	public void Add(int l_in, int r_ex, long v)
 	{
+		if (l_in >= d.Length) return;
 		d[Math.Max(0, l_in)] += v;
 		if (r_ex < d.Length) d[r_ex] -= v;
 	}
--- CSharp/Contests2021/CFR707/C.cs
+++ CSharp/Contests2021/CFR707/C.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class C
 {
@@ -10,42 +8,27 @@ class C
 	{
 		var n = int.Parse(Console.ReadLine());
 		var a = Read();
+
 		const int a_max = 2500000;
-
-		// u+v=u+v が存在する場合
-		{
-			var c = new int[a_max + 1];
-			foreach (var x in a) ++c[x];
-
-			var vs = Enumerable.Range(1, a_max).Where(x => c[x] >= 2).Take(2).ToArray();
-			if (vs.Length == 2)
-			{
-				var i2 = vs.Select(x => Enumerable.Range(0, n).Where(i => a[i] == x).Take(2).ToArray()).ToArray();
-				return $"YES\n{i2[0][0] + 1} {i2[1][0] + 1} {i2[0][1] + 1} {i2[1][1] + 1}";
-			}
-		}
-
-		// 以下、a に同じ値が存在したとしても1組のみ
-		var map = new Dictionary<int, (int, int)>();
+		var set = new (int, int)?[2 * a_max + 1];
 
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = i + 1; j < n; j++)
 			{
 				var key = a[i] + a[j];
-				if (map.ContainsKey(key))
+				if (set[key].HasValue)
 				{
-					var (x, y) = map[key];
+					var (x, y) = set[key].Value;
 					if (x == i || x == j || y == i || y == j) continue;
 					return $"YES\n{x + 1} {y + 1} {i + 1} {j + 1}";
 				}
 				else
 				{
-					map[key] = (i, j);
+					set[key] = (i, j);
 				}
 			}
 		}
-
 		return "NO";
 	}
 }
