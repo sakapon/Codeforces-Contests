5bb405d93dfd9e33ce9433b9ffd9731fd8a7be97
Keiho Sakapon <git@saka-pon.net>
2021-03-21 17:10:03 +0900

    Initialize

---
+++ CSharp/Contests2020/CFR625/A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class A
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	//static void Main() => Console.WriteLine(Solve());
+	//static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var (n2, m) = Read2();
+		var s = Console.ReadLine();
+		var a = Read();
+		var ps = Array.ConvertAll(new bool[n], _ => Read());
+
+		if (n == 0) return "NO";
+		return "YES\n" + string.Join(" ", a);
+	}
+}
