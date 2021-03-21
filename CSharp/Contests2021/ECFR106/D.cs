05f546b1929ec23bc027b2e9287009eff5bad7fd
Keiho Sakapon <git@saka-pon.net>
2021-03-21 13:29:04 +0900

    TLE

--- CSharp/Contests2021/ECFR106/D.cs
+++ CSharp/Contests2021/ECFR106/D.cs
@@ -1,20 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (c, d, x) = Read3();
 
-		return string.Join(" ", a);
+		var r = 0L;
+		foreach (var div in Divisors(x))
+		{
+			var q = Math.DivRem(div + d, c, out var rem);
+			if (rem != 0) continue;
+			r += 1L << Factorize(q).Distinct().Count();
+		}
+		return r;
+	}
+
+	static long[] Divisors(long n)
+	{
+		var r = new List<long>();
+		for (long x = 1; x * x <= n; ++x) if (n % x == 0) r.Add(x);
+		var i = r.Count - 1;
+		if (r[i] * r[i] == n) --i;
+		for (; i >= 0; --i) r.Add(n / r[i]);
+		return r.ToArray();
+	}
+
+	static long[] Factorize(long n)
+	{
+		var r = new List<long>();
+		for (long x = 2; x * x <= n && n > 1; ++x) while (n % x == 0) { r.Add(x); n /= x; }
+		if (n > 1) r.Add(n);
+		return r.ToArray();
 	}
 }
