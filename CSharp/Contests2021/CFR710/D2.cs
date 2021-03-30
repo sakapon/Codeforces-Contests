d052cfeaae2c2405792c81a94680e16710216e47
Keiho Sakapon <git@saka-pon.net>
2021-03-30 22:12:00 +0900

    Improve code

---
+++ CSharp/Contests2021/CFR710/D2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class D2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+
+		var m = a.GroupBy(x => x).Max(g => g.Count());
+		var r = 2 * m - n;
+		return r >= 0 ? r : n % 2;
+	}
+}
--- CSharp/Contests2021/CFR710/G.cs
+++ CSharp/Contests2021/CFR710/G.cs
@@ -4,20 +4,19 @@ using System.Linq;
 
 class G
 {
+	const int max = 1 << 30;
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
 		var s = Console.ReadLine();
 		var n = s.Length;
 
-		if (s.Distinct().Count() == 1) return s[0];
-
 		var d = new Dictionary<char, Queue<int>>();
 		var lastIndexes = new Dictionary<char, int>();
 		for (char c = 'a'; c <= 'z'; c++)
 		{
 			d[c] = new Queue<int>();
-			lastIndexes[c] = -1;
+			lastIndexes[c] = max;
 		}
 
 		for (int i = 0; i < n; i++)
@@ -33,24 +32,23 @@ class G
 		{
 			for (char c = 'z'; c >= 'a'; c--)
 			{
-				if (!d[c].Any()) continue;
+				if (lastIndexes[c] == max) continue;
 
 				if (IsNext(c))
 				{
 					r[i] = c;
 
 					var index = d[c].Dequeue();
-					d[c] = new Queue<int>();
-					lastIndexes[c] = -1;
+					d[c].Clear();
+					lastIndexes[c] = max;
 
 					for (char c2 = 'a'; c2 <= 'z'; c2++)
 					{
-						var q = d[c2];
+						if (lastIndexes[c2] == max) continue;
 
-						while (q.Any() && q.Peek() < index)
-						{
+						var q = d[c2];
+						while (q.Peek() < index)
 							q.Dequeue();
-						}
 					}
 					break;
 				}
@@ -65,7 +63,6 @@ class G
 
 			for (c--; c >= 'a'; c--)
 			{
-				if (lastIndexes[c] == -1) continue;
 				if (lastIndexes[c] < index) return false;
 			}
 			return true;
