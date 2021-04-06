1d774317f727ec5aa852bda0325e38a2a86408e5
Keiho Sakapon <git@saka-pon.net>
2021-04-06 12:03:15 +0900

    AC

--- CSharp/Contests2021/CFR712/E.cs
+++ CSharp/Contests2021/CFR712/E.cs
@@ -3,18 +3,23 @@ using System.Linq;
 
 class E
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long a, long c) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var ps = Array.ConvertAll(new bool[n], _ => Read2L());
 
-		return string.Join(" ", a);
+		var r = ps.Sum(t => t.c);
+		ps = ps.OrderBy(t => t.a).ToArray();
+		var max = ps[0].a;
+
+		foreach (var (a, c) in ps)
+		{
+			if (a > max) r += a - max;
+			max = Math.Max(max, a + c);
+		}
+		return r;
 	}
 }
