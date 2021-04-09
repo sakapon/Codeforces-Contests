b486c5d862e9abf079f66bcfb3a5061d0566eeb0
Keiho Sakapon <git@saka-pon.net>
2021-04-09 16:50:08 +0900

    AC

--- CSharp/Contests2021/CFR703/E.cs
+++ CSharp/Contests2021/CFR703/E.cs
@@ -7,40 +7,44 @@ class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main()
 	{
 		var (n, m) = Read2();
-		var es = Array.ConvertAll(new bool[m], _ => Read3());
+		var es = Array.ConvertAll(new bool[m], _ => (Edge)Read());
+
+		var map = new WeightedMap(n + 1, es, false);
+		var idMap = new List<(int v, int w)>(Enumerable.Range(0, n + 1).Select(i => (i, 0)));
 
 		var id = n;
-		var es2 = new List<Edge>();
 		var used = new int[n + 1, 51];
 
 		int GetId(int v, int w)
 		{
 			if (used[v, w] != 0) return used[v, w];
+			idMap.Add((v, w));
 			return used[v, w] = ++id;
 		}
 
 		foreach (var (u, v, w) in es)
 		{
-			es2.Add(new Edge(u, GetId(v, w), 0));
-			es2.Add(new Edge(v, GetId(u, w), 0));
+			GetId(u, (int)w);
+			GetId(v, (int)w);
 		}
 
-		foreach (var (u, v, w) in es)
+		// 辺を静的に構築すると MLE。
+		var r = ShortestPathCore.Dijkstra(id + 1, v =>
 		{
-			for (int j = 1; j <= 50; j++)
+			if (v <= n)
 			{
-				var w2 = (j + w) * (j + w);
-				if (used[u, j] != 0) es2.Add(new Edge(GetId(u, j), v, w2));
-				if (used[v, j] != 0) es2.Add(new Edge(GetId(v, j), u, w2));
+				return Array.ConvertAll(map[v], e => new Edge(v, used[e.To, e.Cost], 0));
 			}
-		}
+			else
+			{
+				var (v0, w) = idMap[v];
+				return Array.ConvertAll(map[v0], e => new Edge(v, e.To, (w + e.Cost) * (w + e.Cost)));
+			}
+		}, 1);
 
-		var map = new WeightedMap(id + 1, es2.ToArray(), true);
-		var r = map.Dijkstra(1);
 		Console.WriteLine(string.Join(" ", Enumerable.Range(1, n).Select(v => r.GetCost(v))));
 	}
 }
