1a0fdeef4d26507c1e7df117711074d96ea40f48
Keiho Sakapon <git@saka-pon.net>
2021-04-12 02:17:11 +0900

    AC

--- CSharp/Contests2021/CFR714/D.cs
+++ CSharp/Contests2021/CFR714/D.cs
@@ -6,15 +6,56 @@ class D
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var (n, p) = ((int, long))Read2();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		if (a.Any(x => x == 1)) return n - 1;
+
+		var r = (n - 1) * p;
+		var u = new bool[n];
+
+		var q = a.Select((x, i) => (x, i)).Where(t => t.x < p).OrderBy(t => t.x);
+		foreach (var (x, v) in q)
+		{
+			if (u[v]) continue;
+
+			u[v] = true;
+			var c = 1;
+
+			for (int i = v + 1; i < n; i++)
+			{
+				if (a[i] % x != 0) break;
+				if (u[i])
+				{
+					c++;
+					break;
+				}
+				else
+				{
+					u[i] = true;
+					c++;
+				}
+			}
+			for (int i = v - 1; i >= 0; i--)
+			{
+				if (a[i] % x != 0) break;
+				if (u[i])
+				{
+					c++;
+					break;
+				}
+				else
+				{
+					u[i] = true;
+					c++;
+				}
+			}
+
+			r -= (c - 1) * (p - x);
+		}
+		return r;
 	}
 }
