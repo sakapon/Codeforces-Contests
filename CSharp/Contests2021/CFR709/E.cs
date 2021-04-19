be884fa303114b3a39801f5a7d4fd52b3931c116
Keiho Sakapon <git@saka-pon.net>
2021-04-19 14:27:53 +0900

    Refactor

--- CSharp/Contests2021/CFR709/E.cs
+++ CSharp/Contests2021/CFR709/E.cs
@@ -13,66 +13,14 @@ class E
 		var hs = Read();
 		var bs = ReadL();
 
+		(int h, long b)[] hbs = hs.Zip(bs).ToArray();
+
 		// 負の建物が低い建物に含まれる場合の部分和
-		var segs = new List<(int l, int r, long b)>();
+		var segs = NegativeSegments(hbs);
 
-		// 負の建物が右側の低い建物に含まれる場合の部分和
-		// h は単調増加
-		var qr = new Stack<(int l, int h, long b)>();
-		qr.Push((-1, 0, 0));
-
-		for (int i = 0; i < n; i++)
-		{
-			var (h, b) = (hs[i], bs[i]);
-
-			while (qr.Peek().h > h)
-			{
-				var (l0, h0, b0) = qr.Pop();
-				if (b0 < 0) segs.Add((l0, i - 1, b0));
-
-				var (lt, ht, bt) = qr.Pop();
-				qr.Push((lt, ht, bt + b0));
-			}
-
-			if (b >= 0)
-			{
-				var (lt, ht, bt) = qr.Pop();
-				qr.Push((lt, ht, bt + b));
-			}
-			else
-			{
-				qr.Push((i, h, b));
-			}
-		}
-
-		// 負の建物が左側の低い建物に含まれる場合の部分和
-		// h は単調増加
-		var ql = new Stack<(int r, int h, long b)>();
-		ql.Push((n, 0, 0));
-
-		for (int i = n - 1; i >= 0; i--)
-		{
-			var (h, b) = (hs[i], bs[i]);
-
-			while (ql.Peek().h > h)
-			{
-				var (r0, h0, b0) = ql.Pop();
-				if (b0 < 0) segs.Add((i + 1, r0, b0));
-
-				var (rt, ht, bt) = ql.Pop();
-				ql.Push((rt, ht, bt + b0));
-			}
-
-			if (b >= 0)
-			{
-				var (rt, ht, bt) = ql.Pop();
-				ql.Push((rt, ht, bt + b));
-			}
-			else
-			{
-				ql.Push((i, h, b));
-			}
-		}
+		Array.Reverse(hbs);
+		var segs_r = NegativeSegments(hbs);
+		segs.AddRange(segs_r.Select(t => (n - t.r, n - t.l, t.b)));
 
 		var q = new Queue<(int l, int r, long b)>(segs.OrderBy(t => t.l));
 		q.Enqueue((n, n, 0));
@@ -86,10 +34,47 @@ class E
 			while (q.Peek().l == i)
 			{
 				var (l, r, b) = q.Dequeue();
-				dp[r + 1] = Math.Min(dp[r + 1], dp[i] + b);
+				dp[r] = Math.Min(dp[r], dp[i] + b);
 			}
 		}
 
 		return bs.Sum() - dp[n];
+	}
+
+	// 負の建物が右側の低い建物に含まれる場合の部分和
+	// [l, r)
+	static List<(int l, int r, long b)> NegativeSegments((int h, long b)[] hbs)
+	{
+		var n = hbs.Length;
+		var segs = new List<(int l, int r, long b)>();
+
+		// h は単調増加
+		var q = new Stack<(int l, int h, long b)>();
+		q.Push((-1, 0, 0));
+
+		for (int i = 0; i < n; i++)
+		{
+			var (h, b) = hbs[i];
+
+			while (q.Peek().h > h)
+			{
+				var (l0, h0, b0) = q.Pop();
+				if (b0 < 0) segs.Add((l0, i, b0));
+
+				var (lt, ht, bt) = q.Pop();
+				q.Push((lt, ht, bt + b0));
+			}
+
+			if (b >= 0)
+			{
+				var (lt, ht, bt) = q.Pop();
+				q.Push((lt, ht, bt + b));
+			}
+			else
+			{
+				q.Push((i, h, b));
+			}
+		}
+		return segs;
 	}
 }
