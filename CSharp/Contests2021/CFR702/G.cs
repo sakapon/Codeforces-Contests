83b733029369dbdbcc387a6380153754ecd6225c
Keiho Sakapon <git@saka-pon.net>
2021-04-28 23:43:24 +0900

    WA

---
+++ CSharp/Contests2021/CFR702/G.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class G
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static object Solve()
+	{
+		var (n, m) = Read2();
+		var a = Read();
+		var x = Read();
+
+		var s = a.Sum();
+
+		var tsum = 0L;
+		var steps = new List<(int i, long v)> { (-1, 0) };
+
+		for (int i = 0; i < n; i++)
+		{
+			tsum += a[i];
+			if (tsum > steps.Last().v)
+			{
+				steps.Add((i, tsum));
+			}
+		}
+		steps.RemoveAt(0);
+		if (steps.Count == 0) return string.Join(" ", Enumerable.Repeat(-1, m));
+
+		var r = new long[m];
+
+		if (s <= 0)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				var si = First(0, steps.Count, i => steps[i].v >= x[j]);
+				r[j] = si == steps.Count ? -1 : steps[si].i;
+			}
+		}
+		else
+		{
+			for (int j = 0; j < m; j++)
+			{
+				var d = x[j] - steps[^1].v;
+				var k = (d + s - 1) / s;
+				r[j] = k * n;
+
+				x[j] -= (int)(k * s);
+				var si = First(0, steps.Count, i => steps[i].v >= x[j]);
+				r[j] += steps[si].i;
+			}
+		}
+
+		return string.Join(" ", r);
+	}
+
+	static int First(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
+	}
+}
--- CSharp/Contests2021/CFR717/D.cs
+++ CSharp/Contests2021/CFR717/D.cs
@@ -1,20 +1,98 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var (n, qc) = Read2();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
-		return string.Join(" ", a);
+		var afs = Array.ConvertAll(a, x => Factorize(x));
+
+		// a_i の次のグループのインデックス
+		var to = Array.ConvertAll(new bool[n + 1], _ => n);
+		var ps = new bool[100000];
+
+		bool IsCoprime(int i)
+		{
+			foreach (var f in afs[i])
+				if (ps[f]) return false;
+			return true;
+		}
+
+		for (int i = 0, j = 0; i < n; i++)
+		{
+			if (!IsCoprime(i))
+			{
+				for (; j < i; j++)
+				{
+					to[j] = i;
+					foreach (var f in afs[j])
+						ps[f] = false;
+
+					if (Gcd(a[i], a[j]) != 1)
+					{
+						j++;
+						break;
+					}
+				}
+			}
+
+			foreach (var f in afs[i])
+				ps[f] = true;
+		}
+
+		// a_j から 2^i 個目のグループのインデックス
+		var dp = NewArray2<int>(20, n + 1);
+		dp[0] = to;
+
+		for (int i = 0; i < 20 - 1; i++)
+		{
+			for (int j = 0; j <= n; j++)
+			{
+				dp[i + 1][j] = dp[i][dp[i][j]];
+			}
+		}
+
+		// [l, r)
+		int GetCount(int l, int r)
+		{
+			var c = 1;
+			while (l < r)
+			{
+				var i = -1;
+				while (dp[i + 1][l] < r) i++;
+
+				if (i == -1) break;
+				c += 1 << i;
+				l = dp[i][l];
+			}
+			return c;
+		}
+
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		foreach (var (l, r) in qs)
+			Console.WriteLine(GetCount(l - 1, r));
+		Console.Out.Flush();
+	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+	static int Gcd(int a, int b) { for (int r; (r = a % b) > 0; a = b, b = r) ; return b; }
+
+	// Distinct
+	static long[] Factorize(long n)
+	{
+		var r = new List<long>();
+		for (long x = 2; x * x <= n && n > 1; ++x)
+		{
+			if (n % x == 0) r.Add(x);
+			while (n % x == 0) n /= x;
+		}
+		if (n > 1) r.Add(n);
+		return r.ToArray();
 	}
 }
