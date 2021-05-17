84e78e58fafa417af53b7015fd64a9f4a1bfbb25
Keiho Sakapon <git@saka-pon.net>
2021-05-17 10:45:13 +0900

    Improve code

--- CSharp/Contests2021/ECFR109/B.cs
+++ CSharp/Contests2021/ECFR109/B.cs
@@ -1,23 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	//static void Main() => Console.WriteLine(Solve());
-	//static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
 		var a = Read();
 
-		if (a.Select((x, i) => x == i + 1).All(b => b)) return 0;
+		if (Enumerable.Range(1, n).SequenceEqual(a)) return 0;
 		if (a[0] == 1 || a[^1] == n) return 1;
-		if (a[0] == n && a[^1] == 1) return 3;
-		return 2;
+		if (a[0] != n || a[^1] != 1) return 2;
+		return 3;
 	}
 }
--- CSharp/Contests2021/ECFR109/C.cs
+++ CSharp/Contests2021/ECFR109/C.cs
@@ -6,9 +6,6 @@ class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	//static void Main() => Console.WriteLine(Solve());
-	//static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
 	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
@@ -16,66 +13,47 @@ class C
 		var xs = Read();
 		var lrs = Console.ReadLine().Split();
 
-		var ps = Enumerable.Range(0, n).Select(i => (i, x: xs[i], isL: lrs[i][0] == 'L')).OrderBy(_ => _.x).ToArray();
+		var ps = Enumerable.Range(0, n).Select(i => (i, x: xs[i], left: lrs[i][0] == 'L')).OrderBy(_ => _.x).ToArray();
 		var r = Array.ConvertAll(new int[n], _ => -1);
 
-		ForParity(ps.Where(_ => _.x % 2 == 0).ToArray(), m, r);
-		ForParity(ps.Where(_ => _.x % 2 == 1).ToArray(), m, r);
+		ForParity(Array.FindAll(ps, _ => _.x % 2 == 0), m, r);
+		ForParity(Array.FindAll(ps, _ => _.x % 2 == 1), m, r);
 
 		return string.Join(" ", r);
 	}
 
-	static void ForParity((int i, int x, bool isL)[] ps, int m, int[] r)
+	static void ForParity((int, int, bool)[] ps, int m, int[] r)
 	{
-		var q = new Stack<(int i, int x, bool isL)>();
+		var q = new Stack<(int, int, bool)>();
 
 		foreach (var p in ps)
 		{
-			var (i, x, isL) = p;
+			var (j, x, l) = p;
 
-			if (q.Count == 0 || !isL)
+			if (q.Count == 0 || !l)
 			{
 				q.Push(p);
 			}
 			else
 			{
-				var (j, x0, isL2) = q.Pop();
-				if (isL2)
-				{
-					// LL
-					var t = (x + x0) / 2;
-					r[i] = t;
-					r[j] = t;
-				}
-				else
-				{
-					// RL
-					var t = (x - x0) / 2;
-					r[i] = t;
-					r[j] = t;
-				}
+				var (i, x0, l0) = q.Pop();
+
+				// LL, RL
+				var t = (l0 ? x + x0 : x - x0) / 2;
+				r[i] = t;
+				r[j] = t;
 			}
 		}
 
 		while (q.Count >= 2)
 		{
-			var (j, x2, _) = q.Pop();
-			var (i1, x1, isL1) = q.Pop();
+			var (j, x, _) = q.Pop();
+			var (i, x0, l0) = q.Pop();
 
-			if (isL1)
-			{
-				// LR
-				var t = m - (x2 + x1) / 2 + x1;
-				r[i1] = t;
-				r[j] = t;
-			}
-			else
-			{
-				// RR
-				var t = m - (x2 + x1) / 2;
-				r[i1] = t;
-				r[j] = t;
-			}
+			// LR, RR
+			var t = m - (l0 ? x - x0 : x + x0) / 2;
+			r[i] = t;
+			r[j] = t;
 		}
 	}
 }
