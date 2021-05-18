31511c4e02cf4724d44fd31629502d891ce82622
Keiho Sakapon <git@saka-pon.net>
2021-05-18 19:41:14 +0900

    Other AC

--- CSharp/Contests2021/ECFR109/D.cs
+++ CSharp/Contests2021/ECFR109/D.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
@@ -14,26 +11,27 @@ class D
 		var a = Read();
 
 		var rn = Enumerable.Range(0, n).ToArray();
-		var is1 = Array.FindAll(rn, i => a[i] == 1); // i
-		var is0 = Array.FindAll(rn, i => a[i] == 0); // j
+		var is0 = Array.FindAll(rn, i => a[i] == 0); // i
+		var is1 = Array.FindAll(rn, i => a[i] == 1); // j
 
-		var m = is1.Length;
+		var m = is0.Length;
 
-		var dp = new MemoDP2<int>(m + 1, n - m + 1, 1 << 30, (t, i, j) =>
+		var dp = new MemoDP2<int>(m + 1, n - m + 1, -1, (t, i, j) =>
 		{
-			return Math.Min(t[i - 1, j - 1] + Math.Abs(is1[i - 1] - is0[j - 1]), t[i, j - 1]);
+			return Math.Min(t[i - 1, j - 1] + Math.Abs(is0[i - 1] - is1[j - 1]), t[i - 1, j]);
 		});
 
-		for (int j = 0; j <= n - m; j++)
+		for (int i = 0; i <= m; i++)
 		{
-			dp[0, j] = 0;
-		}
-		for (int i = 0; i < m; i++)
-		{
-			dp[i + 1, i + 1] = dp[i, i] + Math.Abs(is1[i] - is0[i]);
+			dp[i, 0] = 0;
+
+			for (int j = i + 1; j <= n - m; j++)
+			{
+				dp[i, j] = 1 << 30;
+			}
 		}
 
-		return Enumerable.Range(m, n - 2 * m + 1).Min(j => dp[m, j]);
+		return dp[m, n - m];
 	}
 }
 
