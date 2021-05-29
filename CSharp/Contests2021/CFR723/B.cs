8ce12526a9670e969117a3d86ea8af7ad349bdfe
Keiho Sakapon <git@saka-pon.net>
2021-05-29 10:53:08 +0900

    AC

--- CSharp/Contests2021/CFR723/B.cs
+++ CSharp/Contests2021/CFR723/B.cs
@@ -1,24 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class B
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	//static void Main() => Console.WriteLine(Solve());
-	//static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
-	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
-	static object Solve()
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve() ? "YES" : "NO")));
+	static bool Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var x = int.Parse(Console.ReadLine());
 
-		if (n == 0) return "NO";
-		return "YES\n" + string.Join(" ", a);
+		if (x >= 11 * 111) return true;
+
+		for (int i = 0; i < 111; i++)
+		{
+			for (int j = 0; j < 11; j++)
+			{
+				if (x == 11 * i + 111 * j) return true;
+			}
+		}
+		return false;
 	}
 }
